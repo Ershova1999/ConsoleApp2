14fd7037a87c064ba6e3600119cbeb4558bbba77
Anastasia <Anastasia@LAPTOP-SKHANDQ9>
2023-03-12 12:13:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HomeWork01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Здравствуйте, " + name);
+        }
+    }
+}
